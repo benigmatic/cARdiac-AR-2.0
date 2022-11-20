ab9daeb0ad0c22c18bd8d9e16c7200a419bd51bb
ATran11 <ANTHONYTRAN1113@YAHOO.COM>
2022-11-19 21:50:18 -0500

    Set up from tutorial

---
+++ cARdiac-AR/Assets/Scripts/ColorChanging.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChanging : MonoBehaviour
+{
+    public Material GreenMaterial;
+    public Material RedMaterial;
+    public Material BlueMaterial;
+    public Material LightBlueMaterial;
+    private Material PreviousMaterial;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        PreviousMaterial = GetComponent<Renderer>().material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TurnRed()
+    {
+        GetComponent<Renderer>().material = RedMaterial;
+
+        PreviousMaterial = RedMaterial;
+    }
+
+    public void TurnBlue()
+    {
+        GetComponent<Renderer>().material = BlueMaterial;
+
+        PreviousMaterial = BlueMaterial;
+    }
+
+    public void TurnGreen()
+    {
+        GetComponent<Renderer>().material = GreenMaterial;
+
+        PreviousMaterial = GreenMaterial;
+    }
+
+    public void TurnLightBlue()
+    {
+        PreviousMaterial = GetComponent<Renderer>().material;
+
+        GetComponent<Renderer>().material = LightBlueMaterial;
+    }
+
+    public void TurnBack()
+    {
+        GetComponent<Renderer>().material = PreviousMaterial;
+    }
+}
